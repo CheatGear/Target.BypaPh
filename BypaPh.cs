8e6ecc555cb907ac6cbd03ff0f2286585281a63f
CorrM <islamnofl.official@gmail.com>
2022-12-07 23:30:27 +0200

    - Update to CG framework (3.1.0)

--- BypaPh.cs
+++ BypaPh.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using CG.Framework.Attributes;
-using CG.Framework.Helper.Platform;
+using CG.Framework.Helper;
 using CG.Framework.Plugin.Memory;
 
 namespace CG.Memory;
@@ -31,34 +28,15 @@ public class BypaPh : MemoryPlugin
 
     [DllImport(LibName, EntryPoint = "RWVM")]
     internal static extern int BypaPH_RWVM(UIntPtr pInstance, UIntPtr baseAddress, [Out] byte[] buffer, UIntPtr bufferSize, out int numberOfBytesReadOrWritten, bool read = true, bool unsafeRequest = false);
-
-
+    
     private UIntPtr _pInstance;
-    private MemoryTargetInfo _targetInfo;
-    private Win32.SystemInfo _sysInfo;
-    private int _memoryBasicInformationSize;
 
     public override Version TargetFrameworkVersion { get; } = new(3, 0, 0);
     public override Version PluginVersion { get; } = new(3, 0, 0);
 
-    private static bool Is64BitProcess(IntPtr processHandle)
-    {
-        return Win32.IsWow64Process(processHandle, out bool retVal) && !retVal;
-    }
-
     private IntPtr GetProcessHandle()
     {
         return BypaPH_GetProcessHandle(_pInstance);
-    }
-
-    private UIntPtr GameStartAddress()
-    {
-        return _sysInfo.MinimumApplicationAddress;
-    }
-
-    private UIntPtr GameEndAddress()
-    {
-        return _sysInfo.MaximumApplicationAddress;
     }
 
     protected override bool OnInit()
@@ -66,19 +44,28 @@ public class BypaPh : MemoryPlugin
         // Using CheatGear as a target it's just to init BypaPH
         _pInstance = BypaPH_ctor((uint)Environment.ProcessId);
 
-        _memoryBasicInformationSize = Marshal.SizeOf<Win32.MemoryBasicInformation>();
-        Win32.GetSystemInfo(out _sysInfo);
-
-        return true;
+        return _pInstance != UIntPtr.Zero;
     }
 
-    protected override bool OnTargetChange(MemoryTargetInfo targetInfo)
+    protected override void OnDispose()
     {
-        _targetInfo = targetInfo;
+        if (_pInstance == UIntPtr.Zero)
+            return;
 
-        BypaPH_ReTarget(_pInstance, (uint)targetInfo.Process.Id);
-        Is64Bit = Is64BitProcess(GetProcessHandle());
+        BypaPH_SetRemoveAllOnExit(_pInstance, true);
+        BypaPH_dtor(_pInstance);
 
+        _pInstance = UIntPtr.Zero;
+    }
+    
+    protected override bool OnTargetChange()
+    {
+        ProcessHandle = GetProcessHandle();
+        if (!IsValidProcessHandle())
+            return false;
+            
+        Is64Bit = UtilsExtensions.Is64BitProcess(ProcessHandle);
+        BypaPH_ReTarget(_pInstance, (uint)CurrentTarget.Process.Id);
         return true;
     }
 
@@ -107,143 +94,5 @@ public class BypaPh : MemoryPlugin
         // STATUS_SUCCESS
         return BypaPH_RWVM(_pInstance, address, buffer, (UIntPtr)(uint)buffer.Length, out numberOfBytesWritten, false) == 0;
     }
-
-    public override MemoryRegionInfo? GetMemoryRegion(UIntPtr address)
-    {
-        // Get Region information
-        bool valid = Win32.VirtualQueryEx(
-            GetProcessHandle(),
-            address,
-            out Win32.MemoryBasicInformation info,
-            (uint)_memoryBasicInformationSize
-        ) == _memoryBasicInformationSize;
-
-        if (!valid)
-            return null;
-
-        var region = new MemoryRegionInfo()
-        {
-            Address = info.BaseAddress,
-            Size = info.RegionSize.ToUInt64(),
-            State = (int)info.State,
-            Protect = (int)info.Protect,
-            Type = (int)info.Type,
-        };
-
-        return region;
-    }
-
-    public override MemoryModuleInfo? GetMainModule()
-    {
-        ProcessModule? processModule;
-
-        try
-        {
-            processModule = _targetInfo.Process.MainModule;
-            if (processModule is null)
-                return null;
-        }
-        catch (Exception)
-        {
-            return null;
-        }
-
-        return new MemoryModuleInfo()
-        {
-            Address = processModule.BaseAddress,
-            Size = (uint)processModule.ModuleMemorySize,
-            Name = Path.GetFileName(processModule.FileName) ?? string.Empty,
-            Path = processModule.FileName ?? string.Empty
-        };
-    }
-
-    public override List<MemoryModuleInfo> GetModulesList()
-    {
-        var ret = new List<MemoryModuleInfo>();
-        // To Avoid Some Games not share it's modules, or could be emulator game
-        try
-        {
-            IntPtr hSnap = Win32.CreateToolhelp32Snapshot(Win32.SnapshotFlags.Module | Win32.SnapshotFlags.Module32, _targetInfo.Process.Id);
-            if (hSnap != Win32.InvalidHandleValue)
-            {
-                var modEntry = new Win32.ModuleEntry32()
-                {
-                    DwSize = (uint)Marshal.SizeOf(typeof(Win32.ModuleEntry32))
-                };
-
-                if (Win32.Module32First(hSnap, ref modEntry))
-                {
-                    do
-                    {
-                        var mod = new MemoryModuleInfo()
-                        {
-                            Handle = modEntry.HModule,
-                            Address = modEntry.ModBaseAddr,
-                            Size = modEntry.ModBaseSize,
-                            Name = modEntry.SzModule,
-                            Path = modEntry.SzExePath
-                        };
-                        ret.Add(mod);
-                    } while (Win32.Module32Next(hSnap, ref modEntry));
-                }
-            }
-
-            Win32.CloseHandle(hSnap);
-        }
-        catch
-        {
-            // Ignore
-        }
-
-        return ret;
-    }
-
-    public override bool IsBadAddress(UIntPtr uIntPtr)
-    {
-        return uIntPtr.ToUInt64() < GameStartAddress().ToUInt64() || uIntPtr.ToUInt64() > GameEndAddress().ToUInt64();
-    }
-
-    public override bool IsValidRemoteAddress(UIntPtr remoteAddress)
-    {
-        // TODO: Very bad when called in hot-path
-        if (remoteAddress == UIntPtr.Zero || IsBadAddress(remoteAddress))
-            return false;
-
-        if (Win32.VirtualQueryEx(GetProcessHandle(), remoteAddress, out Win32.MemoryBasicInformation info, (uint)_memoryBasicInformationSize) != 0)
-            return info.Protect != 0 && (info.Protect & Win32.MemoryProtection.PageNoAccess) == 0;
-
-        return false;
-    }
-
-    public override bool SuspendProcess()
-    {
-        return Win32.NtSuspendProcess(GetProcessHandle()) >= 0;
-    }
-
-    public override bool ResumeProcess()
-    {
-        return Win32.NtResumeProcess(GetProcessHandle()) >= 0;
-    }
-
-    public override bool TerminateProcess()
-    {
-        return Win32.NtTerminateProcess(GetProcessHandle(), 0) >= 0;
-    }
-
-    public override bool IsStaticAddress(UIntPtr address)
-    {
-        // TODO: Implement your logic
-        return false;
-    }
-
-    public override void Dispose()
-    {
-        if (_pInstance == UIntPtr.Zero)
-            return;
-
-        BypaPH_SetRemoveAllOnExit(_pInstance, true);
-        BypaPH_dtor(_pInstance);
-
-        _pInstance = UIntPtr.Zero;
-    }
+    
 }
